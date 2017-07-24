744834be4b9fd026ce1c61f78c3cf9bf66b05d8d
Rodrigo Viana <roviol@gmail.com>
2017-07-23 23:26:46 -0300

    Update mega.py

--- mega.py
+++ mega.py
@@ -14,18 +14,33 @@
 from random import randint
 
 print()
-print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+print('$' * 48)
 print()
 bilhetes = int(input('Quantos bilhetes deseja jogar? '))
 dezenas = int(input('Deseja apostar com 6, 7 ou 8 dezenas? '))
+if dezenas == 6:
+	total = bilhetes * 3.5
+if dezenas == 7:
+	total = bilhetes * 24.5
+if dezenas == 8:
+	total = bilhetes * 98
 print()
 for n in range(1, bilhetes + 1):
 	bilhete = []
 	for z in range(dezenas):
-		bilhete.append(randint(1,60))
+		numero = randint(1,60)
+		for x in range(0, len(bilhete)):
+			if numero == bilhete[x]:
+				numero = randint(1,60)
+		bilhete.append(numero)
+		bilhete.sort()
 	print('Bilhete %d: ' % n, bilhete)
+print()
+print('-' * 48)
+print('Valor total da aposta é R$ %.2f' % total)
+print('-' * 48)
 print()
 print ('               BOA SORTE!!!             ')
 print()
-print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+print('$' * 48)
 print()
